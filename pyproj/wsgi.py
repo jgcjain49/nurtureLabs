908353944e7196059116929adb59db0f431e121d
Jitu <jgcjain49@gmail.com>
2021-05-17 00:20:15 +0530

    global

--- pyproj/wsgi.py
+++ pyproj/wsgi.py
@@ -18,6 +18,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pyproj.settings'
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
 
